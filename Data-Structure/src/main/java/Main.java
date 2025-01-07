4b618bdd7d8e89fd8431f0299a70cd7a07787726
NguyenManhHuy <68103157+huymannhhuy17800@users.noreply.github.com>
2025-01-07 21:16:55 +0700

    Add Prepend, Insert, Remove, Reverse methods

--- Data-Structure/src/main/java/Main.java
+++ Data-Structure/src/main/java/Main.java
@@ -16,12 +16,23 @@ public class Main {
 //        newLL.removeLast();
 //        newLL.removeLast();
 
-        newLL.removeFirst();
-        newLL.removeFirst();
-        newLL.removeFirst();
-        newLL.removeFirst();
-        newLL.removeFirst();
+//        newLL.removeFirst();
+//        newLL.removeFirst();
+//        newLL.removeFirst();
+//        newLL.removeFirst();
+//        newLL.removeFirst();
 
+        newLL.prepend(-1);
+        newLL.prepend(-2);
+
+        newLL.insert(5, 69);
+        newLL.insert(6, 699);
+        newLL.insert(6, 666);
+
+        newLL.remove(6);
+        newLL.remove(6);
+
+        newLL.reverse();
         newLL.print();
     }
 }
--- Data-Structure/src/main/java/data/LinkedList.java
+++ Data-Structure/src/main/java/data/LinkedList.java
@@ -62,11 +62,91 @@ public class LinkedList {
         length -- ;
     }
 
+    public void prepend(int value) {
+        var newNode = new Node(value);
+        if(length == 0) {
+            head = tail = newNode;
+        }else {
+            var temp = head;
+            head = newNode;
+            head.next = temp;
+        }
+        length++;
+    }
+    public void insert (int index, int value) {
+        if(index < 0 || index > length) {
+            System.out.println("Index OUT OF BOUND");
+            return ;
+        }
+        if(length == 0){
+            prepend(value);
+        }else if(index == 0){
+            prepend(value);
+        }else if(index == length) {
+            append(value);
+        }else {
+            var newNode = new Node(value);
+            var temp = head ;
+            var pre = head;
+            for(var i = 0; i < index ; i++){
+                pre = temp;
+                temp = temp.next;
+            }
+            pre.next = newNode;
+            newNode.next = temp;
+            length ++;
+        }
+    }
+
+    public void remove(int index) {
+        if(index < 0 || index >= length){
+            System.out.println("Index Out Of Bound");
+            return ;
+        }
+        if(index == 0) {
+            removeFirst();
+        }else if(index == length - 1) {
+            removeLast();
+        }else {
+            var temp = head;
+            var pre = head;
+            for(var i = 0; i < index ; i++) {
+                pre = temp ;
+                temp = temp.next;
+            }
+            pre.next = temp.next;
+            temp.next = null;
+            length -- ;
+        }
+    }
+
+    public void reverse() {
+        if(length == 0 || length == 1) {
+            System.out.println("Please don't do this with 0 or 1 item on the list");
+            return ;
+        }
+        //Swap head and tail
+        var swap = head ;
+        head = tail;
+        tail = swap;
+
+        var temp = tail ;
+        Node pre = null ;
+        var next = tail ;
+        while(temp != null) {
+            next = temp.next;
+            temp.next = pre;
+            pre = temp;
+            temp = next;
+        }
+    }
+
     public void print(){
         var temp = head;
         while(temp != null ){
-            System.out.print("[" + temp.value +"]");
+            System.out.print("[" + temp.value +"]" + " ");
             temp = temp.next;
         }
+        System.out.println("\nLength: " + length);
     }
 }
