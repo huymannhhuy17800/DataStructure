824023e5730b6e1719a06487f0798986dd96c74a
NguyenManhHuy <68103157+huymannhhuy17800@users.noreply.github.com>
2025-01-07 20:44:48 +0700

    Add append, removeLast, removeFirst for Linked List

--- Data-Structure/src/main/java/Main.java
+++ Data-Structure/src/main/java/Main.java
@@ -1,6 +1,27 @@
+import data.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        LinkedList newLL = new LinkedList(0);
+
+        newLL.append(1);
+        newLL.append(2);
+        newLL.append(3);
+
+//        newLL.removeLast();
+//        newLL.removeLast();
+//        newLL.removeLast();
+//        newLL.removeLast();
+//        newLL.removeLast();
+
+        newLL.removeFirst();
+        newLL.removeFirst();
+        newLL.removeFirst();
+        newLL.removeFirst();
+        newLL.removeFirst();
+
+        newLL.print();
     }
 }
--- Data-Structure/src/main/java/data/LinkedList.java
+++ Data-Structure/src/main/java/data/LinkedList.java
@@ -14,6 +14,54 @@ public class LinkedList {
         length = 1  ;
     }
 
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+            length = 1;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+        length++;
+    }
+
+    public void removeLast() {
+        if(length == 0) {
+            System.out.println("Empty list");
+            return;
+        }
+        if (length == 1) {
+            head = tail = null;
+        }else {
+            var temp = head;
+            var pre = head;
+            while(temp.next != null){
+                pre = temp;
+                temp = temp.next;
+            }
+            tail = pre;
+            tail.next = null;
+        }
+        length -- ;
+    }
+
+    public void removeFirst(){
+        if(length == 0) {
+            System.out.println("Empty list");
+            return ;
+        }
+        if(length == 1) {
+            head = tail = null;
+        }else {
+            var temp = head;
+            head = head.next;
+            temp.next = null;
+        }
+        length -- ;
+    }
+
     public void print(){
         var temp = head;
         while(temp != null ){
